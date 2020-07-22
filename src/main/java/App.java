7162973309623b58eee6c1cd70678cb655edb63e
mbalanya <okobacharles@yahoo.co.uk>
2020-07-22 10:46:59 +0300

    add app.java main routes

---
+++ src/main/java/App.java
@@ -0,0 +1,48 @@
+import com.google.gson.Gson;
+import dao.Sql2oNewsDao;
+import dao.Sql2oUsersDao;
+import dao.Sql2oDepartmentDao;
+import models.Departments;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import static spark.Spark.*;
+
+public class App {
+
+    public static void main(String[] args) {
+        Sql2oUsersDao usersDao;
+        Sql2oDepartmentDao departmentDao;
+        Sql2oNewsDao newsDao;
+        Connection conn;
+        Gson gson = new Gson();
+
+        String connectionString = "jdbc:h2:~/jadle.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        departmentDao = new Sql2oDepartmentDao(sql2o);
+        usersDao = new Sql2oUsersDao(sql2o);
+        newsDao = new Sql2oNewsDao(sql2o);
+        conn = sql2o.open();
+
+
+        post("/departments/new", "application/json", (req, res) -> {
+            Departments departments = gson.fromJson(req.body(), Departments.class);
+            departmentDao.add(departments);
+            res.status(201);
+            res.type("application/json");
+            return gson.toJson(departments);
+        });
+
+        get("/departments", "application/json", (req, res) -> { //accept a request in format JSON from an app
+            res.type("application/json");
+            return gson.toJson(departmentDao.getAll());//send it back to be displayed
+        });
+
+        get("/departments/:id", "application/json", (req, res) -> { //accept a request in format JSON from an app
+            res.type("application/json");
+            int departmentId = Integer.parseInt(req.params("id"));
+            res.type("application/json");
+            return gson.toJson(departmentDao.findById(departmentId));
+        });
+    }
+}
+
